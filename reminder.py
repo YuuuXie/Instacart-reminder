b6b49dd7b55414054b5e1f24e4a16499dfe4dc4e
Yu Shelby Xie <xieyu.fish@gmail.com>
2020-04-18 15:39:35 -0400

    change time

--- reminder.py
+++ reminder.py
@@ -2,13 +2,7 @@ import time, sys, datetime, os
 import smtplib
 from selenium import webdriver
 
-def check(email, email_pwd, account, account_pwd):
-    # login to email
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.ehlo()
-    server.starttls()
-    server.login(email, email_pwd)
-
+def check(account, account_pwd, email, email_pwd):
     # get driver
     url = "https://www.instacart.com"
     driver = webdriver.Chrome(executable_path="../chrome_driver/chromedriver")
@@ -21,11 +15,16 @@ def check(email, email_pwd, account, account_pwd):
     driver.find_element_by_id("nextgen-authenticate.all.log_in_password").send_keys(account_pwd)
     driver.find_element_by_xpath("//button[@type='submit']").click()
 
+    # log in email
+    server = login_email(email, email_pwd)
+    t0 = time.time()
+    
     # check_delivery_times
     while 'Exit' not in os.listdir('./'): # create an Exit file to exit
+
         time.sleep(10)
         driver.find_element_by_xpath("//a[@href='/market-basket/info?tab=delivery']").click()
-        time.sleep(5)
+        time.sleep(10)
 
         try:
             no_delivery_times = driver.find_element_by_xpath("//img[@alt='No delivery times available']")
@@ -38,10 +37,22 @@ def check(email, email_pwd, account, account_pwd):
 
         driver.find_element_by_xpath("//button[@aria-label='Close modal']").click()
         time.sleep(45)
+        if time.time() - t0 > 600: # log in email every 10 min
+            print('log in to the email')
+            server = login_email(email, email_pwd)
+            t0 = time.time()
+
         driver.refresh()
 
     driver.quit()
     
+def login_email(email, password):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login(email, password)
+    return server
+
 
 if __name__ == "__main__":
     check(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])

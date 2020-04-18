9100d4367b9ac7dd0803eb8ebdf25cc38952d5ae
Yu Shelby Xie <xieyu.fish@gmail.com>
2020-04-17 23:54:32 -0400

    created a python code for reminder of available delivery slots

---
+++ reminder.py
@@ -0,0 +1,48 @@
+import requests, time, sys, datetime, os
+import smtplib
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+def check(email, password):
+    # login to email
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login(email, password)
+
+    # get driver
+    url = "https://www.instacart.com"
+    driver = webdriver.Chrome(executable_path="../chrome_driver/chromedriver")
+    driver.get(url)
+    time.sleep(5)
+
+    # log in
+    driver.find_element_by_partial_link_text("Log in").click()
+    driver.find_element_by_id("nextgen-authenticate.all.log_in_email").send_keys(email)
+    driver.find_element_by_id("nextgen-authenticate.all.log_in_password").send_keys(password)
+    driver.find_element_by_xpath("//button[@type='submit']").click()
+
+    # check_delivery_times
+    while 'Exit' not in os.listdir('./'): # create an Exit file to exit
+        time.sleep(10)
+        driver.find_element_by_xpath("//a[@href='/market-basket/info?tab=delivery']").click()
+        time.sleep(5)
+
+        try:
+            no_delivery_times = driver.find_element_by_xpath("//img[@alt='No delivery times available']")
+            print('No delivery times', flush=True)
+        except:
+            time_now = datetime.datetime.now()
+            msg = f"Delivery available: {time_now}"
+            server.sendmail(email, email, msg)
+            print('Yes', time_now, flush=True)
+
+        driver.find_element_by_xpath("//button[@aria-label='Close modal']").click()
+        time.sleep(45)
+        driver.refresh()
+
+    driver.quit()
+    
+
+if __name__ == "__main__":
+    check(sys.argv[1], sys.argv[2])
